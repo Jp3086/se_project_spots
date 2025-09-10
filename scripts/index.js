684d45dbdb62cc459c3f77a8f085f9b341907317
jprieto <jprieto3014@gmail.com>
2025-09-10 18:49:02 -0400

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 
 const newPostBtn = document.querySelector(".profile__new-post-btn");
 const newPostModal = document.querySelector("#new-post-modal");
-const newPostCloseBtn = newPostCloseBtn.querySelector(".modal__close-btn");
+const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
 editProfileBtn.addEventListener("click", function () {
     editProfileModal.classList.add("modal_is-opened");
